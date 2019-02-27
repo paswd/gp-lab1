fe50c027f28bbc7e5d478ee6035183d2a598300c
paswd <paswd@yandex.ru>
2019-02-27 17:20:21 +0300

    First version (not working)

---
+++ Makefile
@@ -0,0 +1,12 @@
+FLAGS=-ccbin clang++-3.8 -std=c++11 --compiler-options -stdlib=libc++ -Wno-deprecated-gpu-targets
+COMPILLER=nvcc
+
+#all: lib start
+all: start
+
+#start: main.o
+#	$(COMPILLER) $(FLAGS) -o da-lab4 main.o -L. lib/lib-z-search.a
+
+start: gp-lab1.cu
+	$(COMPILLER) $(FLAGS) -o gp-lab1 gp-lab1.cu
+
---
+++ gp-lab1.cu
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+__global__ void VectorsPairMaximums(double *first, double *second, double *res) {
+	size_t current = (size_t) threadIdx.x;
+
+	res[current] = max(first[current], second[current]);
+}
+
+__host__ int main(void) {
+	size_t size;
+	cin >> size;
+
+	double *first = new double[size];
+	double *second = new double[size];
+	double *res = new double[size];
+
+	for (size_t i = 0; i < size; i++) {
+		cin >> first[i];
+	}
+	for (size_t i = 0; i < size; i++) {
+		cin >> second[i];
+	}
+
+	double *cudaFirst;
+	double *cudaSecond;
+	double *cudaRes;
+
+	cudaMalloc((void**) &cudaFirst, sizeof(double) * size);
+	cudaMalloc((void**) &cudaSecond, sizeof(double) * size);
+	cudaMalloc((void**) &cudaRes, sizeof(double) * size);
+
+	cudaMemcpy(cudaFirst, first, sizeof(double) * size, cudaMemcpyHostToDevice);
+	cudaMemcpy(cudaSecond, second, sizeof(double) * size, cudaMemcpyHostToDevice);
+
+	VectorsPairMaximums<<<1, size>>>(cudaFirst, cudaSecond, cudaRes);
+
+	cudaEvent_t syncEvent;
+
+	cudaEventCreate(&syncEvent);
+	cudaEventRecord(syncEvent, 0);
+	cudaEventSynchronize(syncEvent);
+
+	cudaMemcpy(res, cudaRes, sizeof(double) * size, cudaMemcpyDeviceToHost);
+
+	cudaEventDestroy(syncEvent);
+	cudaFree(cudaFirst);
+	cudaFree(cudaSecond);
+	cudaFree(cudaRes);
+
+	for (size_t i = 0; i < size; i++) {
+		if (i > 0) {
+			cout << " ";
+		}
+		cout << scientific << res[i];
+	}
+	cout << endl;
+
+	delete [] first;
+	delete [] second;
+	delete [] res;
+
+	return 0;
+}
